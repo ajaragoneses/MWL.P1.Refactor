f7e8334c9ebe9eda83505e53fd3979cde5eea92b
ajaragoneses <aj.aragoneses@gmail.com>
2015-04-09 11:14:19 +0200

    [Refactor] Refactorizando el Case del metodo includes: añadida la creación del objeto a la factoria

--- src/intervals/BothOpenedInterval.java
+++ src/intervals/BothOpenedInterval.java
@@ -5,5 +5,10 @@ public class BothOpenedInterval extends Interval {
 	public BothOpenedInterval(double minimum, double maximum, Opening opening) {
 		super(minimum, maximum, opening);
 	}
+	
+	@Override
+	public boolean includes(double value) {
+		return minimum < value && value < maximum;
+	}
 
 }
--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -4,6 +4,7 @@ package intervals;
 public class IntervalFactory {
 
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
+		if(opening == Opening.BOTH_OPENED) return new BothOpenedInterval(minimum, maximum, opening);
 		return new Interval(minimum, maximum, opening);
 	}
 }
