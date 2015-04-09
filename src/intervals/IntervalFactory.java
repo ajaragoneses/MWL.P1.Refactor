eb3ff2483e6294a4e84d1ae13fc983ef904c3ea6
ajaragoneses <aj.aragoneses@gmail.com>
2015-04-09 11:21:21 +0200

    [Refactor] Añadida la clase RigthOpenedInterval y modificada la factoria en consecuencia

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -6,6 +6,7 @@ public class IntervalFactory {
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
 		if(opening == Opening.BOTH_OPENED) return new BothOpenedInterval(minimum, maximum, opening);
 		if(opening == Opening.LEFT_OPENED) return new LeftOpenedInterval(minimum, maximum, opening);
+		if(opening == Opening.RIGHT_OPENED) return new RightOpenedInterval(minimum, maximum, opening);
 		return new Interval(minimum, maximum, opening);
 	}
 }
