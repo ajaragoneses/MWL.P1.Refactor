4d8b39a269c3d9274b033a4f6acb44ddd1551a47
ajaragoneses <aj.aragoneses@gmail.com>
2015-04-09 11:17:09 +0200

    [Refactor] Añadida la clase LeftOpenedInterval y modificada en consecuencia la factoria

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -5,6 +5,7 @@ public class IntervalFactory {
 
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
 		if(opening == Opening.BOTH_OPENED) return new BothOpenedInterval(minimum, maximum, opening);
+		if(opening == Opening.LEFT_OPENED) return new LeftOpenedInterval(minimum, maximum, opening);
 		return new Interval(minimum, maximum, opening);
 	}
 }
